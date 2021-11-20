3e11dd61e7eba7d8f4d4d38dd99902f9b1f27b6d
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2021-11-19 16:14:18 -0800

    Add files via upload

--- website/gallery.php
+++ website/gallery.php
@@ -1,6 +1,18 @@
 <?php 
 include('config.php');
-include('includes/header.php'); ?>
+include('includes/header.php'); 
+
+// gallery 
+
+// settting up the associative array for the pictures and table data
+$sigmar['stormcast'] = 'storm_The eternal protectors.';
+$sigmar['maggotkin'] = 'maggo_Vile demons from the deep.';
+$sigmar['kharadon'] = 'khara_Drawves of the sky.';
+$sigmar['seraphon'] = 'serph_Lizard men from the stars.';
+$sigmar['soulblight'] = 'soulb_The undead never fades.';
+// $name ..................... $image
+// $key  ..................... $value
+?>
 
     <!-- START of wrapper //////////////////////////////////////////////// -->
 <div id="wrapper">
@@ -8,11 +20,17 @@ include('includes/header.php'); ?>
     <main> <!-- START of main section //////// -->
         <h1><?php echo $headline; ?></h1>
 
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-          dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
-          book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
-
-        <h2>Please fill out our form!</h2>
+        <table>
+        <!-- start of for each loop -->
+        <?php foreach ($sigmar as $name => $image) : ?>
+        <tr>
+            <td><img src="images/<?php echo $name ;?>.JPG" alt="<?php echo str_replace('_', ' ', $name) ;?>"></td>
+            <td><?php echo '<p><b>'.str_replace('_', ' ', $name).'</b></p>' ;?></td>
+            <td><?php echo '<p>'.substr($image, 6).'</p>' ;?></td>
+        </tr>
+        <?php endforeach ; ?>
+        <!-- end of for each loop -->
+    </table>
 
         <!-- insert the form for HW here -->
     </main>
