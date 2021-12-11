cca42b9d12eb73b0220c591d4eb53e275d29e9c3
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2021-12-11 00:00:29 -0800

    Delete credentials.php

--- weeks/week9/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-// credentials page to login into data base on your host site
-
-define('DB_NAME', 'edwinhocodes_it261_db');
-define('DB_USER', 'it261_user');
-define('DB_PASSWORD', 'firstdb123##');
-define('DB_HOST', 'mysql.edwinhocodes.com');
